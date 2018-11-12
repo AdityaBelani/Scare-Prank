db94beb7a6c7f58d488d5b6e700f5398cec0efe9
Aditya Belani <aditbelani@gmail.com>
2018-11-13 02:37:00 +0530

    First Commit

---
+++ Turn Off.bat
@@ -0,0 +1,3 @@
+@echo off
+echo Shutting down in 10 seconds. Please type "shutdown /a" to abort.
+cmd.exe /K shutdown  /t 10 /s /c "Hello Sir!...I'm Mr. Virus! Your computer is going to be destroyed in next 10 seconds!!... Just kidding...its just gonna Shutdown!"
